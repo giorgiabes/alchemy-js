03f19ddbdb4d87f1bc09d74321e4e3a286d159d3
giorgiabes <abesadze.george@gmail.com>
2024-07-05 17:59:14 +0400

    add sumEven

--- tests/arrays.test.js
+++ tests/arrays.test.js
@@ -1,6 +1,7 @@
 const { assert } = require("chai");
 const array = require("../src/arrays/array");
 const hasOne = require("../src/arrays/hasOne");
+const sumEven = require("../src/arrays/sumEven");
 
 describe("array", () => {
   it("should be an array of length 3", () => {
@@ -26,5 +27,19 @@ describe("hasOne", () => {
     assert(!hasOne([]));
     assert(!hasOne([2, 3, 4]));
     assert(!hasOne([6, 7, 8, 9, 10, 11]));
+  });
+});
+
+describe("sumEven", () => {
+  it("should handle a single element array", () => {
+    assert.equal(sumEven([2]), 2);
+  });
+
+  it("should handle an array with a few sequential elements", () => {
+    assert.equal(sumEven([1, 2, 3, 4, 5]), 6);
+  });
+
+  it("should handle a larger array", () => {
+    assert.equal(sumEven([9, 12, 14, 16, 19]), 42);
   });
 });
