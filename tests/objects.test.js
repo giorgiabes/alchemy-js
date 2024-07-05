9d55a339e899a4f4a59b8781c16f685242304b1e
giorgiabes <abesadze.george@gmail.com>
2024-07-05 22:23:44 +0400

    add in objects numberOfPizzas

--- tests/objects.test.js
+++ tests/objects.test.js
@@ -1,5 +1,6 @@
 const { assert } = require("chai");
 const order = require("../src/objects/order");
+const numberOfPizzas = require("../src/objects/numberOfPizzas");
 
 describe("order", () => {
   it("should have a number of pizzas", () => {
@@ -23,5 +24,31 @@ describe("order", () => {
       "make sure to add deliveryInstructions to the order object!"
     );
     assert.equal(typeof order.deliveryInstructions, "string");
+  });
+});
+
+describe.only("order", () => {
+  describe("1 pizzas", () => {
+    const order = {
+      pizzas: 1,
+      extraCheese: true,
+      deliveryInstructions: "Round the back, please!",
+    };
+
+    it("should return the number of pizzas", () => {
+      assert.equal(numberOfPizzas(order), order.pizzas);
+    });
+  });
+
+  describe("5 pizzas", () => {
+    const order = {
+      pizzas: 5,
+      extraCheese: false,
+      deliveryInstructions: "Call when you arrive",
+    };
+
+    it("should return the number of pizzas", () => {
+      assert.equal(numberOfPizzas(order), order.pizzas);
+    });
   });
 });
