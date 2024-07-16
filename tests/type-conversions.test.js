d32fe9a34a6323a73058dc46f2f66841c80ce966
Giorgi <abesadze.george@gmail.com>
2024-07-16 21:10:05 +0400

    combineToString

--- tests/type-conversions.test.js
+++ tests/type-conversions.test.js
@@ -1,4 +1,5 @@
 const toNumber = require('../src/type-conversions/toNumber');
+const combineToString = require('../src/type-conversions/combineToString');
 const { assert } = require('chai');
 
 describe('toNumber', () => {
@@ -14,6 +15,27 @@ describe('toNumber', () => {
             assert.strictEqual(toNumber("a"), 0);
             assert.strictEqual(toNumber("pizza"), 0);
             assert.strictEqual(toNumber("abc123"), 0);
+        });
+    });
+});
+
+describe('combineToString', () => {
+    describe('given two numbers', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(1,1), "11");
+            assert.strictEqual(combineToString(5,10), "510");
+        });
+    });
+    describe('given a number and a string', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(2, "2"), "22");
+            assert.strictEqual(combineToString("5", 55), "555");
+        });
+    });
+    describe('given a number and a boolean', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(2, true), "2true");
+            assert.strictEqual(combineToString(false, 55), "false55");
         });
     });
 });
